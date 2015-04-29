d62a8bf465f952d57a94d64b4026e237b9c3134b
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2015-04-29 10:30:58 -0400

    add non-genesis loop to sectioned templates

--- lib/template/page-simple-sectioned.php
+++ lib/template/page-simple-sectioned.php
@@ -9,4 +9,30 @@ if(function_exists('genesis')){
     genesis();
 } else {
     //not genesis. Do things kind of the old fashioend way.
+    
+get_header(); ?>
+
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+
+        <?php
+        // Start the loop.
+        while ( have_posts() ) : the_post();
+
+            // Include the page content template.
+            get_template_part( 'content', 'page' );
+            MSDSimpleSectionedPage::sectioned_page_output();
+            // If comments are open or we have at least one comment, load up the comment template.
+            if ( comments_open() || get_comments_number() ) :
+                comments_template();
+            endif;
+
+        // End the loop.
+        endwhile;
+        ?>
+
+        </main><!-- .site-main -->
+    </div><!-- .content-area -->
+
+<?php get_footer();
 }
