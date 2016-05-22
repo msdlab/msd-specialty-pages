3d913c4f1a99e577e6a81649a386057e17b98c4d
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2016-05-22 17:10:02 -0400

    iterate version

--- msd-specialty-pages.php
+++ msd-specialty-pages.php
@@ -3,7 +3,7 @@
 Plugin Name: MSD Specialty Pages
 Description: Framework to create specialty templated pages with custom backends. Currently supports two types of LayerCake design structures.
 Author: MSDLAB
-Version: 0.2.5
+Version: 0.2.6
 Author URI: http://msdlab.com
 */
 
